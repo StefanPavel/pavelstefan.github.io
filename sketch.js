0fd7e1a9a9c8dde8ce2b703ae54a87c9453b4c38
StefanPavel <stefan.pavel@omd.com>
2016-05-26 10:50:28 +0300

    test

--- sketch.js
+++ sketch.js
@@ -1,14 +1,24 @@
 var capture;
+var btn;
+var img;
+var cnv;
 
 function setup() {
-  createCanvas(390, 240);
+  cnv = createCanvas(320, 240);
   capture = createCapture(VIDEO);
   capture.size(320, 240);
-
+  capture.hide();
+  btn = createButton("take photo");
+  btn.mousePressed(savePhoto);
 }
 
 function draw() {
-   background(255, 0, 0);
+  background(255);
   image(capture, 0, 0, 320, 240);
+}
 
+function savePhoto()
+{
+    console.log("hgd");
+    saveCanvas(cnv, "aa", "png");
 }
